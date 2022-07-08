76515814ad75c33c44b69792b4dcb1ff661530d2
Cristian-Sosa <64815792+Cristian-Sosa@users.noreply.github.com>
2022-07-08 16:39:50 -0300

    feature: responsive navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function Navbar() {
+
+    const [showMenu, setShowMenu] = useState(1);
+
+    const toggleMenu = () => document.querySelector('.nav-links').classList.toggle('active');
+    
+
+  return (
+    <nav>
+      <img
+        className="nav-toggle"
+        src="./src/assets/icons/list.svg"
+        alt="toggle button"
+        onClick={toggleMenu}
+      />
+
+      <ul className="nav-links">
+        <li className="link">
+          <a className="link-a" href="#">
+            Servicios
+          </a>
+        </li>
+        <li className="link">
+          <a className="link-a" href="#">
+            Voluntariados
+          </a>
+        </li>
+        <li className="link">
+          <a className="link-a" href="#">
+            Contacto
+          </a>
+        </li>
+        <li className="link">
+          <a className="link-a" href="#">
+            Testimonios
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
